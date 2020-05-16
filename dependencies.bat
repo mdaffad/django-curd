4976c8bba2ca864aaffc30d8cb3a293218d930af
mdaffad <13518141@std.stei.itb.ac.id>
2020-05-16 16:33:37 +0700

    gitignore autoremove and update dependencies.bat

--- dependencies.bat
+++ dependencies.bat
@@ -1,7 +1,8 @@
-pip install venv curd
+python -m venv curd
 cd curd/Scripts
 activate
 cd ../..
 virtualenv --clear venv
 pip install -r requirements.txt
 pip install mysqlclient-1.4.6-cp38-cp38-win32.whl
+git rm -r curd
